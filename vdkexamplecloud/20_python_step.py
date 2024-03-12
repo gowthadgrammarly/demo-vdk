01aefe36cd7f69a08f5cc09eda23810ddfd40bb5
root <root@myvdk-dep-6887fcd65d-6xvhf>
2024-03-12 11:34:11 +0000

    Update data job: vdkexamplecloud
    reason

--- vdkexamplecloud/20_python_step.py
+++ vdkexamplecloud/20_python_step.py
@@ -21,6 +21,6 @@ def run(job_input: IJobInput):
     log.info(f"Starting job step {__name__}")
 
     # Write your python code inside here ... for example:
-    job_input.send_object_for_ingestion(
-        payload=dict(id="Hello World!"), destination_table="hello_world"
-    )
+    # job_input.send_object_for_ingestion(
+    #     payload=dict(id="Hello World!"), destination_table="hello_world"
+    #)
