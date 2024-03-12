eedbe6249cbfb32ddd0e5a318e403ea99ad331f4
root <root@gd-vdk-dep-58cf754dd6-sfrxk>
2024-03-12 08:52:24 +0000

    Update data job: example
    reason

--- example/10_sql_step.sql
+++ example/10_sql_step.sql
@@ -9,4 +9,5 @@
 -- A valid query parameter looks like → {parameter}.
 -- Parameters will be automatically replaced if there is a corresponding value existing in the IJobInput properties.
 
-CREATE TABLE IF NOT EXISTS hello_world (id NVARCHAR);
+--CREATE TABLE IF NOT EXISTS hello_world (id NVARCHAR);
+SELECT * FROM hello_world;
