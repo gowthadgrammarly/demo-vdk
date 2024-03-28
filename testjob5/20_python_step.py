f6a199fef358e6bf808d7f5721e840968eeb270a
gdhamodharan <>
2024-03-28 07:08:44 +0000

    Update data job: testjob5
    reason
    
    Signed-off-by: gdhamodharan

--- testjob5/20_python_step.py
+++ testjob5/20_python_step.py
@@ -1,8 +1,9 @@
 # Copyright 2023-2024 Broadcom
 # SPDX-License-Identifier: Apache-2.0
 import logging
-from datetime import datetime
 
+import datetime
+from datetime import datetime as datet
 from vdk.api.job_input import IJobInput
 
 log = logging.getLogger(__name__)
